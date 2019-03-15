dfd0ae376a0d2398c31ceb3608645588e72fe865
Mubeezi <victor.muwanga@gmail.com>
2019-03-15 11:09:20 +0300

    part-two

--- age.js
+++ age.js
@@ -1,12 +1,16 @@
 
- var d = "2000-10-09";
+// var d = "2000-10-09";
 
 // var d = new Date();
 
-list = d.split('-');
+//list = d.split('-');
 
-yr = list[0];
+//yr = list[0];
 
-age = 2019 - parseInt(yr);
+//age = 2019 - parseInt(yr);
 
-console.log(age);
+//console.log(age);
+
+const ageCalc=(x,y)=>
+  y.slice(6,10)-x.slice(6,10);
+    console.log(ageCalc("10-09-2000","03-15-2019"))
