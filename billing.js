fd9a8e31b01faf1e0e7f9659bb1d996519793c47
Mubeezi <victor.muwanga@gmail.com>
2019-03-15 10:59:16 +0300

    part-one

--- billing.js
+++ billing.js
@@ -1,10 +1,12 @@
 
-/
-const transcationCharge = 0.025;
+
+//const transcationCharge = 0.025;
 //determines the transaction charge//
 
-  var amountTendered = 100000;
+//  var amountTendered = 100000;
 
-  console.log( 'This is what the billed amount is (less transaction charges)');
-   var billed_amount = amountTendered -(transcationCharge*amountTendered) ;
-  console.log( billed_amount );
+  //console.log( 'This is what the billed amount is (less transaction charges)');
+   //var billed_amount = amountTendered -(transcationCharge*amountTendered) ;
+  //console.log( billed_amount );
+  const bill=(x,y)=>x-x*y;
+   console.log(bill(100000,0.025));
