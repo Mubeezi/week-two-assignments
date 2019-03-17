8ca9e48b41eb9bbcd9750dfef62342617d2c7d8f
Mubeezi <victor.muwanga@gmail.com>
2019-03-17 16:39:26 +0300

    update

--- log-even.js
+++ log-even.js
@@ -1,5 +1,13 @@
-   for ( i = 2; i<= 20; i++) 
+/*   for ( i = 2; i<= 20; i++)
       {if (i%2===0) {console.log(i)
       }
 
    }
+*/
+let evens = (min, max) => {
+	for (var i = min; i <= max; i++) {
+		i%2 === 0? console.log(i):null;
+	}
+}
+
+evens(2,20);
