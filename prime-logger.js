2bcb76a00b1c170efdcadfac746f209178e76afa
Mubeezi <victor.muwanga@gmail.com>
2019-03-17 17:01:28 +0300

    update

--- prime-logger.js
+++ prime-logger.js
@@ -1,4 +1,4 @@
-var n=100;
+/*var n=100;
 for (var num = 2; num <= n; num++) {
 
   var a = false;//not prime
@@ -12,4 +12,21 @@ for (var num = 2; num <= n; num++) {
   }
 }
 
-var num=1;
+var num=1;*/
+const number = (n) =>
+{
+  for (var num = 2; num <= n; num++) {
+
+    var a = false;
+    for (var i = 2; i <= num; i++) {
+        if (num%i===0 && i!==num) {
+            a = true;
+        }
+    }
+    if (a === false) {
+    console.log(num);
+
+  }
+    }
+  }
+  console.log(number(100));
