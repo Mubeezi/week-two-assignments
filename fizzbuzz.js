0bbc6dfe80d4fa2b0146f4bbbcbb2c90e5da6076
Mubeezi <victor.muwanga@gmail.com>
2019-03-18 17:27:55 +0300

    update

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,4 +1,4 @@
-  var a=1
+/*  var a=1
   while(a<=100){
     if(a%3===0 && a%5===0){
       console.log("fizzbuzz");
@@ -11,4 +11,20 @@
       console.log(a);
     }
     a++;
-  }
+  }*/
+
+  var fizzBuzz = num => {
+for (var n =1; n <= num; n++){
+if (n%3===0 && n%5===0){
+console.log("FizzBuzz!");
+}else if (n%3===0) {
+console.log("Fizz!");
+}else if (n%5===0){
+console.log("Buzz!");
+}else{
+console.log(n);
+}
+}
+return ("finished!");
+}
+fizzBuzz (100)
